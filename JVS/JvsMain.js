d17b80649a4c27af9c55d34bf98bc7894dc2bad6
IsraelRvm <Tekken.gr@gmail.com>
2022-11-02 11:54:54 -0400

    ww2

---
+++ JVS/JvsMain.js
@@ -0,0 +1,7 @@
+document.querySelector('.Boton-menu').addEventListener('click',()=>{
+document.querySelector('.main-menu').classList.toggle('show');
+});
+ScrollReveal().reveal('.img-prin');
+ScrollReveal().reveal('.Fotos',{ delay: 500 });
+ScrollReveal().reveal('.Banner',{ delay: 500 });
+ScrollReveal().reveal('.Banner2',{ delay: 500 });
