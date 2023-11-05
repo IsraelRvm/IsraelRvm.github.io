b9d9b46a858a83159732b0c04b9d334ca9554933
IsraelRvm <Tekken.gr@gmail.com>
2023-11-04 22:24:05 -0400

    dsfsdf

--- JVS/JvsMain.js
+++
@@ -1,7 +0,0 @@
-document.querySelector('.Boton-menu').addEventListener('click',()=>{
-document.querySelector('.main-menu').classList.toggle('show');
-});
-ScrollReveal().reveal('.img-prin');
-ScrollReveal().reveal('.Fotos',{ delay: 500 });
-ScrollReveal().reveal('.Banner',{ delay: 500 });
-ScrollReveal().reveal('.Banner2',{ delay: 500 });
